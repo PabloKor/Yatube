40c342858eef504c5fba1925ca19605688e25b7e
Pavel Korolev <batos1254@gmail.com>
2022-02-28 19:47:35 +0300

    Create profile, post, post_edit

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post, Group
+from .models import Post, Group, User
 from .forms import PostForm
 from django.core.paginator import Paginator
 
@@ -35,3 +35,36 @@ def new_post(request):
         return render(request, 'posts/new.html', {'form': form})
     form = PostForm()
     return render(request, 'posts/new.html', {'form': form})
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    posts = author.posts.order_by('-pub_date')
+    paginator = Paginator(posts, 12)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    context = {'author': author,
+               'page': page,
+               'paginator': paginator}
+    return render(request, 'posts/profile.html', context=context)
+
+
+def post_view(request, username, post_id):
+    post = get_object_or_404(Post, author__username=username, id=post_id)
+    author = post.author
+
+    return render(request, 'posts/post.html', {'post': post, 'author': author})
+
+
+@login_required
+def post_edit(request, username, post_id):
+    post = get_object_or_404(Post, author__username=username, id=post_id)
+    if post.author != request.user:
+        return redirect('post', username=username, post_id=post_id)
+    form = PostForm(request.POST or None,
+                    files=request.FILES or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('post', username=username, post_id=post_id)
+    return render(request, 'posts/post_new.html', {'form': form,
+                                                   'post': post})
